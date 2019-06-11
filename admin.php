2d2e16e3b0bfdf7731905dd758c4fe32abac3d09
edwin254e <30731575+edwin254e@users.noreply.github.com>
2019-06-11 23:29:36 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -99,5 +99,6 @@ document.getElementById("sidebar").classList.toggle('active');
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script>(function (d) { var n = d.createElement("script"); n.src = "https://www.storeya.com/externalscript/storeyaall/735A8A20"; n.type = "text/javascript"; d.getElementsByTagName("head")[0].appendChild(n); })(document);</script>
 </body>
 </html>
